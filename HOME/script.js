36a553df134b8b431b52e303a478fe843fe4d5ff
Julia Alves <juliaa.brito22@gmail.com>
2024-03-07 19:40:48 -0300

    últimas notícias

--- HOME/script.js
+++ HOME/script.js
@@ -13,3 +13,14 @@ document.getElementById('btnMenu').addEventListener('click', function(){
     }
 });
 
+
+
+/* todos os elementos com a classe 'movie' */
+const elementos = document.getElementsByClassName('movie');
+
+for (let i = 0; i < elementos.length; i++) {
+    elementos[i].addEventListener('click', function() {
+        window.location.href = '../Em Cartaz/EmCartaz.html';
+    });
+}
+
