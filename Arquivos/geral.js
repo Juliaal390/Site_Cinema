b8aa6c422cf6fe87f1df4e867d5d9cd2a3e12379
Julia Alves <juliaa.brito22@gmail.com>
2024-03-08 21:41:47 -0300

    arquivos organizados

---
+++ Arquivos/geral.js
@@ -0,0 +1,17 @@
+document.getElementById('btnMenu').addEventListener('click', function(){
+    let menuBarr = document.getElementById('menu');
+    let screenWidth = window.innerWidth; // largura da janela do navegador
+
+    if(screenWidth <= 660){
+        if (menuBarr.classList.contains('show-menu')) {
+            menuBarr.classList.remove('show-menu'); // remove a classe 
+        } else {
+            menuBarr.classList.add('show-menu'); // adiciona a classe
+        }
+    } else {
+        menuBarr.classList.remove('show-menu'); // fecha o menu burguer ao aumentar a tela
+    }
+});
+
+
+
